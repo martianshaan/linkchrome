0629eddccb3319a3a0543216de94e4336658e4b2
martianshaan <shantanu.kale@viit.ac.in>
2024-09-28 08:38:40 +0530

    update:removed

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
-import LinkedInMessageInput from '../../components/LinkedInMessageInput';
 import './style.css'
 
 function App() {
   return (
     <div className="p-4 bg-blue-900">
       <h1>LinkAI</h1>
-      <LinkedInMessageInput />
     </div>
   );
 }
